6e27697f50a710553d928c91a7694cd9ef220e69
bibek-jaiswal045 <bibekjaiswalpersonal045@gmail.com>
2026-08-27 17:40:13 +0545

    refactor: remove GitHub Actions environment variables from build step and next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-
-let basePath = "";
-let assetPrefix = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "") ?? "";
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}`;
-}
-
 const nextConfig = {
   output: "export",
-  basePath,
-  assetPrefix,
   images: {
     unoptimized: true,
   },
